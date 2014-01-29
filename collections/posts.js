bc7b5c5bb706cf43945f1544fa94baee69060e77
sseppola <sseppola@me.com>
2014-01-29 16:41:55 +1100

    added comment about the Posts collection

--- collections/posts.js
+++ collections/posts.js
@@ -1,1 +1,3 @@
+// Notice that there is not variable declaration. Meteor makes this a
+// global variable for the whole app by emitting the variable declaration
 Posts = new Meteor.Collection('posts');
