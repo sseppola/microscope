3af78d5bcefb0191d2f6598331e361fb7760957e
sseppola <sseppola@me.com>
2014-02-03 18:36:25 +1100

    Added edit posts form

--- client/views/posts/post_item.js
+++ client/views/posts/post_item.js
@@ -1,5 +1,11 @@
 // post_item.html manager
 Template.postItem.helpers({
+	ownPost: function () {
+		if (Meteor.userId())
+			return this.userId == Meteor.userId();
+		else 
+			return false;
+	},
 	domain: function () {
 		var a = document.createElement('a');
 		a.href = this.url;
