f76defe5bcad789f6a773bbefea6dbe261099d5e
sseppola <sseppola@me.com>
2014-02-11 20:09:06 +1100

    added load next button

--- client/views/posts/posts_list.js
+++ client/views/posts/posts_list.js
@@ -1,5 +1,8 @@
 // posts_list.html manager
 
 Template.postsList.helpers({
-	
+	hasMorePosts: function () {
+		this.posts.rewind();
+		return Router.current().postsLimit() == this.posts.fetch().length;
+	}
 });
