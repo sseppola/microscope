e83e68cd504b5e889a8964a1b05c5936646a7c3b
sseppola <sseppola@me.com>
2014-02-12 14:36:25 +1100

    added upvote buttons

--- collections/posts.js
+++ collections/posts.js
@@ -36,11 +36,30 @@ Meteor.methods({
 			userId: user._id,
 			author: user.username,
 			submitted: new Date().getTime(),
-			commentsCount: 0
+			commentsCount: 0,
+			upvoters: [],
+			votes: 0
 		});
 
 		var postId = Posts.insert(post);
 
 		return postId;
+	},
+	upvote: function (postId) {
+		var user = Meteor.user();
+		if (!user)
+			throw new Meteor.Error(401, "You need to log in to upvote");
+
+		var post = Posts.findOne(postId);
+		if (!post)
+			throw new Meteor.Error(422, 'Post not found');
+
+		if (_.include(post.upvoters, user._id))
+			throw new Meteor.Error(422, 'Already upvoted this post');
+
+		Posts.update(post._id, {
+			$addToSet: {upvoters: user._id},
+			$inc: {votes: 1}
+		});
 	}
 });
--- server/fixtures.js
+++ server/fixtures.js
@@ -18,8 +18,8 @@ if (Posts.find().count() === 0) {
 		author: sacha.profile.name,
 		url: 'http://sachagreif.com/introducing-telescope',
 		submitted: now - 7 * 3600 * 1000,
-		commentsCount: 2
-
+		commentsCount: 2,
+		upvoters: [], votes: 0
 	});
 
 	Comments.insert({
@@ -45,7 +45,8 @@ if (Posts.find().count() === 0) {
 		author: tom.profile.name,
 		url: 'http://meteor.com',
 		submitted: now - 10 * 3600 * 1000,
-		commentsCount: 0
+		commentsCount: 0,
+		upvoters: [], votes: 0
 	});
 
 	Posts.insert({
@@ -54,7 +55,8 @@ if (Posts.find().count() === 0) {
 		author: tom.profile.name,
 		url: 'http://themeteorbook.com',
 		submitted: now - 12 * 3600 * 1000,
-		commentsCount: 0
+		commentsCount: 0,
+		upvoters: [], votes: 0
 	});
 
 	for (var i = 0; i < 10; i++) {
@@ -64,7 +66,8 @@ if (Posts.find().count() === 0) {
 			userId: sacha._id,
 			url: 'http://google.com/?q=test-' + i,
 			submitted: now - i * 3600 * 1000,
-			commentsCount: 0
+			commentsCount: 0,
+		upvoters: [], votes: 0
 		});
 	}
 }
